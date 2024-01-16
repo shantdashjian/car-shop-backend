24eeb51c0d6c053b092d04236966ca06b1147fe7
Shant Dashjian <shantgdashjian@gmail.com>
2024-01-16 14:35:52 +0400

    Add logger and set logging level

--- src/main/java/com/example/carshopbackend/CarShopBackendApplication.java
+++ src/main/java/com/example/carshopbackend/CarShopBackendApplication.java
@@ -1,13 +1,18 @@
 package com.example.carshopbackend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarShopBackendApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(CarShopBackendApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(CarShopBackendApplication.class, args);
+        logger.info("Application started!");
     }
 
 }
