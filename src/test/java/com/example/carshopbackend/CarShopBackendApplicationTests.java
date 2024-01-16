656af3806ddb514a8c1b23efe1a951713acdf64e
Shant Dashjian <shantgdashjian@gmail.com>
2024-01-16 14:02:11 +0400

    Add initial project setup

---
+++ src/test/java/com/example/carshopbackend/CarShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
