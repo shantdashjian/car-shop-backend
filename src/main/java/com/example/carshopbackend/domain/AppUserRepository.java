6f94c8e95bb42375931cd89c09af8ab197ec7380
Shant Dashjian <shantgdashjian@gmail.com>
2024-01-19 09:04:26 +0400

    Protect appUsers endpoint

--- src/main/java/com/example/carshopbackend/domain/AppUserRepository.java
+++ src/main/java/com/example/carshopbackend/domain/AppUserRepository.java
@@ -1,9 +1,11 @@
 package com.example.carshopbackend.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(exported = false)
 public interface AppUserRepository extends CrudRepository<AppUser, Long> {
     Optional<AppUser> findByUsername(String username);
 }
